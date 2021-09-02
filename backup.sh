66c0f0a9fe36596c4d34828723c7a35b2d103eb2
Riley A <riley@akariley.net>
2021-09-02 13:26:14 -0500

    change backup script to use vars

--- backup.sh
+++ backup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #set -euo pipefail
-BASEDIR=/rust-backups
+BASEDIR=/game-backups
 RUSTDIR=rust
-PREFIX=$USER'-backup'
-DIR=$PREFIX-`date +%F`
-FILENAME=$PREFIX'-'`date +%H%M`
+PREFIX=$USER
+DIR=${PREFIX}/`date +%m`
+FILENAME=$PREFIX'-'`date +%Y-%b-%d-%H%M`
 FULLNAME=${BASEDIR}/${DIR}/${FILENAME}.tar.gz
 MKNICE='ionice -c 3'
 
@@ -19,6 +19,10 @@ backuplist=(
   ${HOME}/${RUSTDIR}/lgsm/config-lgsm/rustserver
   ${HOME}/${RUSTDIR}/serverfiles/server/rustserver
   ${HOME}/${RUSTDIR}/log
+  ${HOME}/${RUSTDIR}/backup.sh
+  ${HOME}/${RUSTDIR}/restore.sh
+  ${HOME}/${RUSTDIR}/weekly-tasks.sh
+  ${HOME}/${RUSTDIR}/lootlogs-maint.sh
 )
 
 if [[ -d ${HOME}/${RUSTDIR}/serverfiles/oxide ]]
@@ -28,29 +32,29 @@ if [[ -d ${HOME}/${RUSTDIR}/serverfiles/oxide ]]
   )
 fi
 
-
-# echo $DIR
 echo
-# echo $FILENAME
-# echo
-# echo
-# echo $FULLNAME
 if [ $(/usr/bin/mount | grep -c ${BASEDIR}) != 1 ]
   then
   # Directory not mounted... try and mount.
-  ${MKNICE} /usr/bin/mount /rust-backups/ || exit 1
+  ${MKNICE} /usr/bin/mount /game-backups/ || exit 1
 fi
+
 if [[ -d ${BASEDIR}/${DIR} ]]
   then
   echo "Directory ${BASEDIR}/${DIR} exists."
 else
   echo "Directory ${BASEDIR}/${DIR} does not exist... making it."
-  ${MKNICE} mkdir --mode=700 ${BASEDIR}/${DIR} || exit 1
+  ${MKNICE} mkdir -p --mode=700 ${BASEDIR}/${DIR} || exit 1
 fi
 # Directory made... proceed.
+
+# check if the server is running; if so, save.
+if pgrep RustDedicated > /dev/null
+# server is running
+then
+  timeout 5 /usr/bin/webrcon-cli ${RCONIP}:${RCONPORT} ${RCONPASSWORD} "server.save"
+fi
+
+sleep 2
 echo "Making ${FULLNAME}"
-
-
-# ${MKNICE} tar zcvf $FULLNAME "${HOME}/${RUSTDIR}/lgsm/config-lgsm/rustserver" "${HOME}/${RUSTDIR}/serverfiles/server/rustserver" "${HOME}/${RUSTDIR}/log" "${HOME}/${RUSTDIR}/serverfiles/oxide"
 ${MKNICE} tar zcvf $FULLNAME "${backuplist[@]}"
-

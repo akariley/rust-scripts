be6b19724435f305f23372f859f515e15e4aaaee
Riley A <riley@akariley.net>
2021-09-12 17:02:29 -0500

    ident fixup

--- restore.sh
+++ restore.sh
@@ -36,7 +36,7 @@ fi
 # 2 = server running
 
 if [[ -z $1 ]]
-  then
+then
   # no params -- display help
   echo
   echo "Syntax: $0 [backupfile] <date (XX); pad single digits with a prededing 0 (ie, March is '03')>"
@@ -52,88 +52,75 @@ if [[ $1 == "list" ]]
 then
   # echo 'in list loop'
   if [[ -z $2 ]]
-    then
+  then
     ls -1 ${BACKUPDIR}/${BACKUPDIRPREFIX}/
-    exit
+  else
+    ls -1 ${BACKUPDIR}/${2}
   fi
-  ls -1 ${BACKUPDIR}/${2}
-  exit
 fi
 
-
-
-  # we're in the homedir, continue.
-  # echo 'in homedir'
-  # echo 'Deleting test dir...'
-  # rm -rv ${HOME}/testdir/
-  # mkdir --mode=700 ${HOME}/testdir || exit 1
-  
-  
-  # got a backup file, check if it's another day's
-  # TODO: proper regex for format.
-  if [[ -z $2 ]]
-    then
-    # check for lock files.
-    if [[ -e ${INSTALLDIR}/lgsm/lock/rustserver.lock ]]
-      then
-      echo "Error: Server is running.  Stop it first. (and make a backup!)"
-      exit 2
-    fi
-    # no date, assuming today
-    if [[ -e ${BACKUPDIR}/${BACKUPDIRPREFIX}/$1 ]]
-      then
-      echo
-      echo "Extracting from ${BACKUPDIR}/${BACKUPDIRPREFIX}/$1..."
-      echo
-      for backuppath in "${BACKUPLIST[@]}"
-      do
-      echo "Extract $backuppath?"
-      select yn in "Yes" "No"; do
-        case $yn in
-          Yes ) echo "tar zxvf ${BACKUPDIR}/${BACKUPDIRPREFIX}/$1 --strip-components=2 $backuppath -C ${INSTALLDIR}" ; break;;
-          No ) break;;
-        esac
-        done
-      done
-      echo
-    else
-      echo "Error: ${BACKUPDIR}/${BACKUPDIRPREFIX}/$1 does not exist.  Did you input the correct date?"
-      exit 1
-    fi
-  echo
-  else
-    # they put a file and a date, extract from it instead of $today    
-    
-    # check for lock files.
-    if [[ -e ${INSTALLDIR}/lgsm/lock/rustserver.lock ]]
-      then
-      echo "Error: Server is running.  Stop it first. (and make a backup!)"
-      exit 2
-    fi
-    
-    
-    
-    if [[ -e ${BACKUPDIR}/${2}/${1} ]]
-      then
-      echo
-      echo "Extracting from ${BACKUPDIR}/${2}/${1}..."
-      echo
-      for backuppath in "${BACKUPLIST[@]}"
-      do
-      echo "Extract $backuppath?"
-      select yn in "Yes" "No"; do
-        case $yn in
-          Yes ) echo "tar zxvf ${BACKUPDIR}/${2}/${1} --strip-components=2 $backuppath -C ${INSTALLDIR}" ; break;;
-          No ) break;;
-        esac
-        done
-      done
-      echo
-    else
-      echo "Error: ${BACKUPDIR}/${2}/${1} does not exist.  Did you input the correct date?"
-      exit 1
-    fi
+# got a backup file, check if it's another day's
+# TODO: proper regex for format.
+if [[ -z $2 ]]
+then
+# check for lock files.
+  if [[ -e ${INSTALLDIR}/lgsm/lock/rustserver.lock ]]
+  then
+    echo "Error: Server is running.  Stop it first. (and make a backup!)"
+    exit 2
+  fi
+  # no date, assuming today
+  if [[ -e ${BACKUPDIR}/${BACKUPDIRPREFIX}/$1 ]]
+  then
     echo
-    exit
-    fi # end date / file check loop
+    echo "Extracting from ${BACKUPDIR}/${BACKUPDIRPREFIX}/$1..."
+    echo
+    for backuppath in "${BACKUPLIST[@]}";
+    do
+      echo "Extract $backuppath?"
+      select yn in "Yes" "No"; do
+      case $yn in
+        Yes ) echo "tar zxvf ${BACKUPDIR}/${BACKUPDIRPREFIX}/$1 --strip-components=2 $backuppath -C ${INSTALLDIR}" ; break;;
+        No ) break;;
+      esac
+    done
+    echo
+  else
+    echo "Error: ${BACKUPDIR}/${BACKUPDIRPREFIX}/$1 does not exist.  Did you input the correct date?"
+    exit 1
+  fi
+  echo
+else
+  # they put a file and a date, extract from it instead of $today    
+  # check for lock files.
+  if [[ -e ${INSTALLDIR}/lgsm/lock/rustserver.lock ]]
+  then
+    echo "Error: Server is running.  Stop it first. (and make a backup!)"
+    exit 2
+  fi
+
+
+
+  if [[ -e ${BACKUPDIR}/${2}/${1} ]]
+  then
+    echo
+    echo "Extracting from ${BACKUPDIR}/${2}/${1}..."
+    echo
+    for backuppath in "${BACKUPLIST[@]}"
+    do
+      echo "Extract $backuppath?"
+      select yn in "Yes" "No"; do
+      case $yn in
+        Yes ) echo "tar zxvf ${BACKUPDIR}/${2}/${1} --strip-components=2 $backuppath -C ${INSTALLDIR}" ; break;;
+        No ) break;;
+      esac
+    done
+    echo
+  else
+    echo "Error: ${BACKUPDIR}/${2}/${1} does not exist.  Did you input the correct date?"
+    exit 1
+  fi
+  echo
+  exit
+fi  # end date / file check loop
 

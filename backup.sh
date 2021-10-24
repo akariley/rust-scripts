c332b945c9dd04780a60f09d4e0245fbdb2fac9d
Riley A <riley@akariley.net>
2021-10-24 08:18:34 -0500

    move instance backup outside case loop for better processing

--- backup.sh
+++ backup.sh
@@ -16,29 +16,12 @@ function script_exit {
 
 trap script_exit exit
 
-tmpFile=$(mktemp --tmpdir=${scriptDir}/tmp instancelist.${rs_selfName}.XXXXXX)
+tmpFile=$(mktemp --tmpdir=${scriptDir}/tmp instancelist.${rs_selfName}.XXXXXXXX)
 cd ${scriptDir}
 
 
 fullBackup=0
 mkNice='ionice -c 3'
-
-if [[ "$#" -eq 1 ]] || [[ "$#" -eq 0 ]]
-then
-  # zero or one option entered -- lets see if it's help.
-  if [[ ${1} == '--help' ]] || [[ ${1} == '-h' ]]
-  then
-    echo "One input: help goes here."
-    exit 3
-  else
-    if [[ -z ${1} ]]
-    then
-      echo "Zero input given: help goes here."
-      exit 3
-    fi
-  fi
-fi
-
 
 while [ "$#" -gt 0 ]
 do
@@ -62,50 +45,9 @@ do
           echo "Error: ${installDir}/${1} does not exist."
         else
           lgsmConfig=${installDir}/lgsm/config-lgsm/rustserver/${1}.cfg
-
-          # let's snag the rcon stuff.
-
-          rconIp=$(awk -F'=' '/[Ii][Pp]="?([0-9]{1,3}[\.]){3}[0-9]{1,3}"?/ {print $2}' ${lgsmConfig} | tr -d '"')
-          rconPort=$(awk -F'=' '/^[Rr][Cc][Oo][Nn][Pp][Oo][Rr][Tt]="?\d{0,5}"?/ {print $2}' ${lgsmConfig} | tr -d '"')
-          rconPassword=$(awk -F'=' '/^[Rr][Cc][Oo][Nn][Pp][Aa][Ss]{2}[Ww][Oo][Rr][Dd]="?[[:alnum:]]{0,63}"?/ {print $2}' ${lgsmConfig} | tr -d '"')
-
-          instanceBackupList=(
-            lgsm/config-lgsm/rustserver/${1}.cfg
-            lgsm/config-lgsm/rustserver/secrets-${1}.cfg
-            lgsm/config-lgsm/rustserver/common.cfg
-            serverfiles/server/${1}
-          )
-
-          fileName=${user}_${1}_${backupDate}
-
-          if [[ -z ${backupDirSuffix} ]]
-          then
-            # no prefix so omit the var
-            fullName=${backupDir}/${fileName}.tar.gz
-            trueBackupDir=${backupDir}
-          else
-            fullName=${backupDir}/${backupDirSuffix}/${fileName}.tar.gz
-            trueBackupDir=${backupDir}/${backupDirSuffix}
-          fi
-
-          if [[ -d ${trueBackupDir}/ ]]
-            then
-            echo "Directory ${trueBackupDir}/ exists."
-          else
-            echo "Directory ${trueBackupDir}/ does not exist... making it."
-            ${mkNice} mkdir -p --mode=700 ${trueBackupDir}/
-          fi
-
-          # do we need to save the server?
-          if [[ -e ${installDir}/lgsm/lock/${1}.lock ]]
-          then
-            timeout 5 ${webRconCmd} ${rconIp}:${rconPort} ${rconPassword} "server.save"
-          fi
-          tar zcvf $fullName -C ${installDir} "${instanceBackupList[@]}"
-          # let's sleep for a bit to avoid save churning.
-          sleep 1
-        fi # end install dir check
-      fi # end $1 check
+          echo ${1} >> $tmpFile
+        fi # end instance validation
+      fi # end input checking
       ;;
   esac
   shift
@@ -123,12 +65,54 @@ then
     fullName=${backupDir}/${backupDirSuffix}/${fileName}.tar.gz
     trueBackupDir=${backupDir}/${backupDirSuffix}
   fi
-
   echo "Making ${fullName}"
   ${mkNice} tar zcvf $fullName -C ${installDir} "${backupList[@]}"
+  exit
+else
+  while read instanceName
+  do
+    # let's snag the rcon stuff.
+    lgsmConfig=${installDir}/lgsm/config-lgsm/rustserver/${instanceName}.cfg
 
+    rconIp=$(awk -F'=' '/[Ii][Pp]="?([0-9]{1,3}[\.]){3}[0-9]{1,3}"?/ {print $2}' ${lgsmConfig} | tr -d '"')
+    rconPort=$(awk -F'=' '/^[Rr][Cc][Oo][Nn][Pp][Oo][Rr][Tt]="?\d{0,5}"?/ {print $2}' ${lgsmConfig} | tr -d '"')
+    rconPassword=$(awk -F'=' '/^[Rr][Cc][Oo][Nn][Pp][Aa][Ss]{2}[Ww][Oo][Rr][Dd]="?[[:alnum:]]{0,63}"?/ {print $2}' ${lgsmConfig} | tr -d '"')
 
+    instanceBackupList=(
+      lgsm/config-lgsm/rustserver/${instanceName}.cfg
+      lgsm/config-lgsm/rustserver/secrets-${instanceName}.cfg
+      lgsm/config-lgsm/rustserver/common.cfg
+      serverfiles/server/${instanceName}
+    )
 
+    fileName=${user}_${instanceName}_${backupDate}
 
+    if [[ -z ${backupDirSuffix} ]]
+    then
+      # no prefix so omit the var
+      fullName=${backupDir}/${fileName}.tar.gz
+      trueBackupDir=${backupDir}
+    else
+      fullName=${backupDir}/${backupDirSuffix}/${fileName}.tar.gz
+      trueBackupDir=${backupDir}/${backupDirSuffix}
+    fi
+
+    if [[ -d ${trueBackupDir}/ ]]
+      then
+      echo "Directory ${trueBackupDir}/ exists."
+    else
+      echo "Directory ${trueBackupDir}/ does not exist... making it."
+      ${mkNice} mkdir -p --mode=700 ${trueBackupDir}/
+    fi
+
+    # do we need to save the server?
+    if [[ -e ${installDir}/lgsm/lock/${instanceName}.lock ]]
+    then
+      timeout 5 ${webRconCmd} ${rconIp}:${rconPort} ${rconPassword} "server.save"
+    fi
+    tar zcvf $fullName -C ${installDir} "${instanceBackupList[@]}"
+    # let's sleep for a bit to avoid save churning.
+    sleep 1
+  done < $tmpFile
 
 fi

9f6686880eafc691032f825792b2ddce0cbf2715
Riley A <riley@akariley.net>
2021-11-01 15:27:25 -0500

    correct order for redirection

--- rs-wipe-script.sh
+++ rs-wipe-script.sh
@@ -295,11 +295,11 @@ echo "Wipe cycle start: $(date +"%c")"
 if [[ ${wipeDoRestartServer} -eq 1 ]]
 then
   echo "Sending restart command to server via rcon..."
-  timeout 2 ${webRconCmd} ${rconIp}:${rconPort} ${rconPassword} "restart ${wipeRestartSeconds} '${wipeRestartReason}'" 2>&1 >/dev/null
+  timeout 2 ${webRconCmd} ${rconIp}:${rconPort} ${rconPassword} "restart ${wipeRestartSeconds} '${wipeRestartReason}'" > /dev/null 2>&1
   while [[ 1 -eq 1 ]]
   do
     # server running.
-    timeout --preserve-status 2 ${webRconCmd} ${rconIp}:${rconPort} ${rconPassword} 'playerlist' 2>&1 >/dev/null
+    timeout --preserve-status 2 ${webRconCmd} ${rconIp}:${rconPort} ${rconPassword} 'playerlist' > /dev/null 2>&1
     if [[ ! $? -eq 143 ]]
     then
       # server is down
